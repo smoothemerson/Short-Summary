19de7c34edad0a678e85267b35661a6656844430
emerson <emerson8096@gmail.com>
2024-01-27 11:28:16 -0300

    refactor: formatting

--- web/server.js
+++ web/server.js
@@ -1,5 +1,5 @@
-import axios from "https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm"
+import axios from "https://cdn.jsdelivr.net/npm/axios@1.3.5/+esm";
 
 export const server = axios.create({
-    baseURL: "http://localhost:3333",
-})
+  baseURL: "http://localhost:3333",
+});
