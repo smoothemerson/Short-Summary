35ebaaf9d1aa4101fc160e37f29478f7c47e61b4
emerson <emerson8096@gmail.com>
2024-01-27 11:26:52 -0300

    refactor: formatting

--- server/utils/summary.js
+++ server/utils/summary.js
@@ -1,1 +1,2 @@
-export const summaryExample = "O JavaScript é uma linguagem de programação voltada para o desenvolvimento web."
+export const summaryExample =
+  "O JavaScript é uma linguagem de programação voltada para o desenvolvimento web.";
