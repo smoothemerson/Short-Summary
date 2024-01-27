fa85eb4afe5ceea9bcc21e1fbb01cdf17b504680
smoothemerson <emerson8096@gmail.com>
2024-01-27 12:27:08 -0300

    refactor: formatting

--- web/form.js
+++ web/form.js
@@ -1,31 +1,31 @@
-import { server } from "./server.js"
-const form = document.querySelector("#form")
-const input = document.querySelector("#url")
-const content = document.querySelector("#content")
+import { server } from "./server.js";
+const form = document.querySelector("#form");
+const input = document.querySelector("#url");
+const content = document.querySelector("#content");
 
 form.addEventListener("submit", async (event) => {
-    event.preventDefault()
-    content.classList.add("placeholder")
+  event.preventDefault();
+  content.classList.add("placeholder");
 
-    const videoURL = input.value
-    
-    /*if(!videoURL.includes("shorts")){
+  const videoURL = input.value;
+
+  /*if(!videoURL.includes("shorts")){
         return content.textContent = "Esse vídeo não parece ser um short."
     }*/
 
-    const [_, params] = videoURL.split("/shorts/")
-    const [videoID] = params.split("?si")
+  const [_, params] = videoURL.split("/shorts/");
+  const [videoID] = params.split("?si");
 
-    content.textContent = "Obtendo o texto do áudio..."
-    
-    const transcription = await server.get("/summary/" + videoID)
+  content.textContent = "Obtendo o texto do áudio...";
 
-    content.textContent = "Realizando o resumo..."
+  const transcription = await server.get("/summary/" + videoID);
 
-    const summary = await server.post("/summary", {
-        text: transcription.data.result,
-    })
+  content.textContent = "Realizando o resumo...";
 
-    content.textContent = summary.data.result
-    content.classList.remove("placeholder")
-})
+  const summary = await server.post("/summary", {
+    text: transcription.data.result,
+  });
+
+  content.textContent = summary.data.result;
+  content.classList.remove("placeholder");
+});
